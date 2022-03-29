605a87a693bd4ab1b620367c171c50f9f0bd2299
Marius <marius@example.com>
2022-03-29 16:59:05 +0300

    add new works

--- src/components/NewTodoForm/NewTodoForm.jsx
+++ src/components/NewTodoForm/NewTodoForm.jsx
@@ -1,5 +1,26 @@
-function NewTodoForm() {
-  return <div>NewTodoForm</div>;
+import { useState } from 'react';
+
+function NewTodoForm(props) {
+  const [newTodoTitle, setNewTodoTitle] = useState('');
+
+  function inputHandler(e) {
+    setNewTodoTitle(e.target.value);
+  }
+
+  function formHandler(e) {
+    e.preventDefault();
+    props.onAddTodo(newTodoTitle);
+    setNewTodoTitle('');
+  }
+
+  return (
+    <form onSubmit={formHandler} className='add-item'>
+      <button className='submit' type='submit'>
+        <i className='fa fa-plus-circle' aria-hidden='true'></i>
+      </button>
+      <input onChange={inputHandler} value={newTodoTitle} type='text' placeholder='Add todo' />
+    </form>
+  );
 }
 
 export default NewTodoForm;
--- src/components/TodoListPage/TodoListPage.jsx
+++ src/components/TodoListPage/TodoListPage.jsx
@@ -2,6 +2,7 @@ import './todoListPage.css';
 import NewTodoForm from '../NewTodoForm/NewTodoForm';
 import TodoHeader from '../TodoHeader/TodoHeader';
 import TodoList from '../TodoList/TodoList';
+import { useState } from 'react';
 
 const initTodos = [
   {
@@ -22,13 +23,32 @@ const initTodos = [
 ];
 
 function TodoListPage() {
+  const [todosArr, setTodosArr] = useState(initTodos);
+  const [currentId, setCurrentId] = useState(4);
+  function onNewTodo(newTodoTitle) {
+    console.log('newTodoTitle ===', newTodoTitle);
+
+    setCurrentId((prevState) => prevState + 1);
+
+    const newItem = {
+      id: currentId,
+      title: newTodoTitle,
+      isDone: false,
+    };
+    // console.log('[newItem, ...todosArr] ===', [...todosArr, newItem]);
+    // setTodosArr([...todosArr, newItem]);
+    // nes state priklauso nuo pries tai buvusio state
+    setTodosArr((prevState) => [...prevState, newItem]);
+  }
+  function onDelete(id) {}
+
   return (
     <div className='body'>
       <div className='container'>
         <TodoHeader />
         <div className='content'>
-          <TodoList items={initTodos} />
-          <NewTodoForm />
+          <TodoList items={todosArr} />
+          <NewTodoForm onAddTodo={onNewTodo} />
         </div>
       </div>
     </div>
