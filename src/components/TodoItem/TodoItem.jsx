ad303a4887a84da539191f857528842a421d4111
Marius <marius@example.com>
2022-03-29 16:03:43 +0300

    check uncheck

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
 function TodoItem(props) {
   const [title, setTitle] = useState('I am TodoItem');
-  //fa-circle-thin // nepazymetas
-  // fa-check-circle // pazymetas
-  const checkClasses = props.item.isDone === true ? 'fa fa-check-circle' : 'fa fa-circle-thin';
-  const itemClasses = props.item.isDone === true ? 'item line-through' : 'item';
+  const [isChecked, setIsChecked] = useState(props.item.isDone);
+
+  const checkClasses = isChecked === true ? 'fa fa-check-circle' : 'fa fa-circle-thin';
+  const itemClasses = isChecked === true ? 'item line-through' : 'item';
+
+  function checkHandler() {
+    // jei naujas state priklauso nuo buvusio turetumem atnaujinti su funkcija
+    setIsChecked((prevState) => !prevState);
+  }
+
   return (
     <li className={itemClasses}>
-      <i className={checkClasses} aria-hidden='true'></i>
+      <i onClick={checkHandler} className={checkClasses} aria-hidden='true'></i>
       <span className='text'> {props.item.title} </span>
       <i className='fa fa-pencil edit-icon' aria-hidden='true'></i>
       <i className='fa fa-trash delete-icon' aria-hidden='true'></i>
